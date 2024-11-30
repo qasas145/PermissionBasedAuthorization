d9e63bde70db5b80b0075dcececed51bfa50493f
qasas145 <mohamed.sayed14527@yahoo.com>
2024-11-30 18:19:25 +0200

    First approach done

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,11 +2,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 public  class ProductsController : Controller {
-    // [Permission("Permission.Products.Create")]
     [HttpGet]
     [Authorize("Permission.Products.Create")]
     public async Task<IActionResult> TestApi() {
-        Console.WriteLine("we are in the controller");
         return View();
     }
     [HttpGet]
